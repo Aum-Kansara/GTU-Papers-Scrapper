ca1aaef44118d963f7b6b6de858373d2ed8f8f1d
Aum Kansara <aumkan23@gmail.com>
2023-07-24 20:05:44 +0530

    created required directories

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,7 @@ import os
 
 years=["W2022","W2021","W2020","W2019","W2018","W2017","S2023","S2023","S2022","S2021","S2020","S2019","S2018"]
 
-subject_codes=["3170701"]
+subject_codes=["3170701","2170701"]
 courses=["AF","BA","BB","BC","BD","BE","BESP","BH","BI","BL","BM","BN","BP","BPSP","BT","BV","CS","DA","DB","DH","DI","DISP","DM","DP","DS","DV","EP","FD","HM","IB","IC","IM","MA","MB","MC","MCSP","MD","ME","MH","ML","MN","MP","MR","MS","MT","MV","PB","PD","PH","PM","PP","PR","TE"]
 
 save_folder_path=r"D:\ITM\Sem 6\Information Extraction Workshop\Project\Project\static\Compiler Design"
